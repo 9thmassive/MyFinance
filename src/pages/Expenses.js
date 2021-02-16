f48e4804d47c9957e0b581be6f7f03afd86fccda
Arthur <62344195+9thmassive@users.noreply.github.com>
2021-02-16 22:58:09 +0400

    Update Expenses.js

--- src/pages/Expenses.js
+++ src/pages/Expenses.js
@@ -1,55 +1,250 @@
-import React from 'react'
-import { DropdownButton, Dropdown } from 'react-bootstrap'
+import React, { useState, useRef } from 'react'
+import { DropdownButton, Dropdown, Form } from 'react-bootstrap'
+import { AgGridReact, AgGridColumn } from 'ag-grid-react'
+import { toast } from 'react-toastify'
+import firebase from 'firebase'
+import 'ag-grid-community/dist/styles/ag-grid.css'
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css'
+import Rating from 'react-simple-star-rating'
 import './exp.css'
+const expList = [
+    {
+        nameExp: 'Auto insurance.',
+    },
+    {
+        nameExp: 'Health insurance.',
+    },
+    {
+        nameExp: 'Car payment.',
+    },
+    {
+        nameExp: 'Child care',
+    },
+    {
+        nameExp: 'utility',
+    },
+    {
+        nameExp: 'Other',
+    },
+]
+
 function Expenses() {
-    const expList = [
-        {
-            nameExp: 'Auto insurance.',
-        },
-        {
-            nameExp: 'Health insurance.',
-        },
-        {
-            nameExp: 'Car payment.',
-        },
-        {
-            nameExp: 'Child care',
-        },
-        {
-            nameExp: 'Public transportation',
-        },
-        {
-            nameExp: 'Gasoline',
-        },
-        {
-            nameExp: `Electricity and natural gas`,
-        },
-        {
-            nameExp: 'Internet',
-        },
-        {
-            nameExp: 'Other',
-        },
-    ]
+    toast.configure()
+    const uid = firebase.auth().currentUser.uid
+
+    const [group, setGroup] = useState(false)
+    const [rating, setRating] = useState(0)
+    const [dropDownVal, setDropDownVal] = useState('Select Expenses Group')
+    const [dorpVal, stDropVal] = useState(expList)
+    const [expData, setExpData] = useState([{}])
+    const [rowData, setRowData] = useState([
+        { make: 'Toyota', model: 'Celica', price: 35000 },
+        { make: 'Ford', model: 'Mondeo', price: 32000 },
+        { make: 'Porsche', model: 'Boxter', price: 72000 },
+    ])
+
+    //-------------------
+    const groupNameRef = useRef()
+    const nameRef = useRef()
+    const priceRef = useRef()
+    //-------------------
+
+    const toDay = () => new Date().toLocaleDateString()
+
+    let dat = {
+        '15/01/2020': [
+            {
+                name: 'oil',
+                group: 'Car',
+                value: 2400,
+            },
+            {
+                name: 'oil',
+                group: 'Car',
+                value: 2400,
+            },
+        ],
+    }
+
+    function userMessage(num, msg) {
+        if (num === 1) {
+            return toast.info(msg, {
+                position: 'bottom-right',
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })
+        }
+        if (num === 2) {
+            return toast.success(msg, {
+                position: 'bottom-right',
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })
+        }
+        if (num === 3) {
+            return toast.error(msg, {
+                position: 'bottom-right',
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })
+        }
+    }
+    //
+    async function handleAddExpenses() {
+        if (parseInt(priceRef.current.value) !== +priceRef.current.value) {
+            return userMessage(3, 'Please add only number in a price')
+        }
+
+        await firebase
+            .firestore()
+            .collection('expenses')
+            .doc(uid)
+            .get()
+            .then((doc) => {
+                if (doc.exists) {
+                    setExpData(() => doc.data()[toDay()])
+                }
+            })
+        debugger
+        await firebase
+            .firestore()
+            .collection('expenses')
+            .doc(uid)
+            .set({
+                [toDay()]: [
+                    ...expData,
+                    {
+                        name: nameRef.current.value,
+                        value: priceRef.current.value,
+                        priority: rating,
+                    },
+                ],
+            })
+
+        console.log(expData)
+        return userMessage(1, 'Added successFull')
+    }
+
     return (
         <div className="container ">
-            <div className="container add">
-                <DropdownButton
-                    id="dropdown-item-button"
-                    title="Dropdown button"
+            <div className="container ">
+                <div className="container add">
+                    {group ? (
+                        <DropdownButton id="mainColor" title={dropDownVal}>
+                            {dorpVal.map(({ nameExp }, index) => {
+                                return (
+                                    <Dropdown.Item
+                                        key={index}
+                                        className="drop-hover"
+                                        as="button"
+                                        onClick={(e) => {
+                                            setDropDownVal(e.target.outerText)
+                                        }}
+                                    >
+                                        {nameExp}
+                                    </Dropdown.Item>
+                                )
+                            })}
+                        </DropdownButton>
+                    ) : (
+                        <Form.Control
+                            ref={groupNameRef}
+                            className="expInput"
+                            placeholder="Group name"
+                        />
+                    )}
+
+                    <button
+                        className="btn mainColor expLeft "
+                        onClick={() => {
+                            setGroup(!group)
+                            if (!group) {
+                                if (
+                                    dorpVal.find(
+                                        (el) =>
+                                            el.nameExp ===
+                                            groupNameRef.current.value
+                                    )
+                                ) {
+                                    return userMessage(
+                                        1,
+                                        '😕 The group has already been added, you cannot add twice'
+                                    )
+                                }
+                                stDropVal((prev) => [
+                                    ...prev,
+                                    {
+                                        nameExp: groupNameRef.current.value,
+                                    },
+                                ])
+                            }
+                        }}
+                    >
+                        {group ? '+Create new Group' : 'Add group'}
+                    </button>
+                </div>
+
+                <div className="add namPrice">
+                    <Form.Control
+                        className="expInput expLeft"
+                        placeholder="Name"
+                        ref={nameRef}
+                    />
+                    <Form.Control
+                        className="expInput expLeft"
+                        placeholder="Price"
+                        ref={priceRef}
+                    />
+                    <Form.Check className="expLeft expCheck" />
+                </div>
+                <span className="txtc">Cost Priority</span>
+                <div className="rate">
+                    <Rating
+                        onClick={(rate) => {
+                            setRating(rate)
+                        }}
+                        ratingValue={rating}
+                        size={25}
+                        label
+                        transition
+                        fillColor="orange"
+                        emptyColor="gray"
+                    />
+                </div>
+                <button
+                    className="btn mainColor w-50"
+                    onClick={handleAddExpenses}
                 >
-                    {expList.map(({ nameExp }, index) => {
-                        return (
-                            <Dropdown.Item as="button">{nameExp}</Dropdown.Item>
-                        )
-                    })}
-                    <Dropdown.Item as="button">Action</Dropdown.Item>
-                </DropdownButton>
-                <button className="btn btn-success">+Add Group</button>
+                    Add List
+                </button>
             </div>
-            <div className="listt"></div>
+
+            <div className="listt ">
+                <br />
+                <div
+                    className="ag-theme-alpine"
+                    style={{ height: 400, width: 600 }}
+                >
+                    <AgGridReact rowData={dat[`15/01/2020`]}>
+                        <AgGridColumn field="name"></AgGridColumn>
+                        <AgGridColumn field="value"></AgGridColumn>
+                        <AgGridColumn field="group"></AgGridColumn>
+                    </AgGridReact>
+                </div>
+            </div>
         </div>
     )
 }
-
 export default Expenses
